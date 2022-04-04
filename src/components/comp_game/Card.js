5c9f4dadf095e02ca46fbe60ecf92e385a102c1c
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-04 17:37:31 -0600

    all functionalities done
    Solo no enseña la segunda carta cuando no coincide. Pendiente de arreglar

--- src/components/comp_game/Card.js
+++ src/components/comp_game/Card.js
@@ -22,69 +22,62 @@ import BackImage from '../card_visuals/0_backSide_Card.png'
 export default class Card extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props)
       this.state = {
-      isFlipped: false,
-      solved: false
-    };
-    this.clickHandler = this.clickHandler.bind(this);
+        isFlipped: false
+      };
     this.renderFrontImageCard = this.renderFrontImageCard.bind(this);
 
   }
 
-  clickHandler($event) {
-    $event.preventDefault();
-    console.log("Vuelta")
-    this.state.isFlipped = !this.state.isFlipped     
+      
 
-  }
   renderFrontImageCard(){
     switch(this.props.cardid) {
       case 1:
-        return <img src={CaptainFalconImage} alt="Chica Image" />
+        return <img src={CaptainFalconImage} alt="Chica Image" className="front"/>
       case 2:
-        return <img src={FoxImage} alt="Fox image" />
+        return <img src={FoxImage} alt="Fox image" className="front"/>
       case 3:
-        return <img src={DevilImage} alt="Devil"/>
+        return <img src={DevilImage} alt="Devil"className="front"/>
       case 4:
-        return <img src={CupheadYMugmanImage} alt="Cup Head"/>
+        return <img src={CupheadYMugmanImage} alt="Cup Head"className="front"/>
       case 5:
-        return <img src={EndermanImage} alt="Enderman"/> 
+        return <img src={EndermanImage} alt="Enderman" className="front"/> 
       case 6:
-        return <img src={SteveImage} alt="Steve" />
+        return <img src={SteveImage} alt="Steve" className="front"/>
       case 7:
-        return <img src={FreddyImage} alt="Freddy" />
+        return <img src={FreddyImage} alt="Freddy" className="front"/>
       case 8:
-        return <img src={ChicaImage} alt="Chica"/>
+        return <img src={ChicaImage} alt="Chica" className="front"/>
       case 9:
-        return <img src={JettImage} alt="Jett"/>
+        return <img src={JettImage} alt="Jett" className="front"/>
       case 10:
-          return <img src={KillJoyImage} alt="Killjoy"/>
+          return <img src={KillJoyImage} alt="Killjoy" className="front"/>
       case 11:
-        return <img src={NuezImage} alt="Nuez" />
+        return <img src={NuezImage} alt="Nuez" className="front"/>
       case 12:
-        return <img src={PatapumImage} alt="Patapum" />
+        return <img src={PatapumImage} alt="Patapum" className="front"/>
       case 13:
-        return <img src={SansImage} alt="Sans"/>
+        return <img src={SansImage} alt="Sans" className="front"/>
       case 14:
-        return <img src={PapyrusImage} alt="Papyrus"/>
+        return <img src={PapyrusImage} alt="Papyrus" className="front"/>
       case 15:
-        return <img src={QuanChiImage} alt="Quan Chi"/>
+        return <img src={QuanChiImage} alt="Quan Chi" className="front"/>
       case 16:
-        return <img src={ScorpionImage} alt="Scorpion"/>
+        return <img src={ScorpionImage} alt="Scorpion" className="front"/>
       default:
-        return   <img src={BackImage} alt="BacksideCardImage" />
+        return   <img src={BackImage} alt="BacksideCardImage" className="front"/>
   }
   }
   
   render() {
+    this.state.isFlipped = this.props.flip   
     return (
       <ReactCardFlip isFlipped={this.state.isFlipped} className='card'  flipDirection='horizontal'>
-        <div className='card' onClick={this.clickHandler}>
-
-          <img src={BackImage} alt="BacksideCardImage" />
+        <div className='card' onClick={this.clickHandler} >
+          <img src={BackImage} alt="BacksideCardImage"  className={this.props.cardid}/>
         </div>
-        <div className='card' onClick={this.clickHandler}>
+        <div className='card' >
         {this.renderFrontImageCard()}
         </div>
       </ReactCardFlip >
--- src/components/comp_game/Game.js
+++ src/components/comp_game/Game.js
@@ -4,23 +4,8 @@ import Card from './Card'
 import logo from './assets/memory_game_pixel.png'
 import { Link } from 'react-router-dom'
 import random_memory_setup from './random_game_setup'
-import CaptainFalconImage from '../card_visuals/cFalcon.png'
-import FoxImage from '../card_visuals/fox.png'
-import DevilImage from '../card_visuals/devil.png'
-import CupheadYMugmanImage from '../card_visuals/cupheadMugman.png'
-import EndermanImage from '../card_visuals/enderman.png'
-import SteveImage from '../card_visuals/steve.png'
-import FreddyImage from '../card_visuals/freddy.png'
-import ChicaImage from '../card_visuals/chica.png'
-import JettImage from '../card_visuals/jett.png'
-import KillJoyImage from '../card_visuals/kj.png'
-import NuezImage from '../card_visuals/nuez.png'
-import PatapumImage from '../card_visuals/patapum.png'
-import SansImage from '../card_visuals/sans.png'
-import PapyrusImage from '../card_visuals/papyrus.png'
-import QuanChiImage from '../card_visuals/quanChi.png'
-import ScorpionImage from '../card_visuals/scorpion.png'
-
+import YouWin from './assets/YouWin.png'
+import BackToMenu from './assets/BackToMenu.png'
 const cards_info = require ('./cards_info')
 
 class Game extends React.Component {
@@ -32,12 +17,72 @@ class Game extends React.Component {
       setup: random_memory_setup(),
       oldPoints: 0,
       points: 0,
-      id_first_card_flipped: 0,
-      id_second_card_flipped: 0, 
+      win: false, 
+      first_card_flipped: null,
+      solved: [],
+      flipped: new Map(),
+      coincidio: null
     }
-} 
-  render() {
+    this.clickRegisterCardSelected = this.clickRegisterCardSelected.bind(this);
+    this.resetFlip = this.resetFlip.bind(this)
     console.log(this.state.setup)
+    this.state.setup.map(id => this.state.flipped.set(id, false))
+
+  }
+  resetFlip(firstId, secondId){
+    this.state.flipped.set(firstId, !this.state.flipped.get(firstId))
+    this.state.flipped.set(secondId, !this.state.flipped.get(secondId))
+/*     console.log(secondId,this.state.flipped.get(secondId))
+    console.log(firstId, this.state.flipped.get(firstId)) */
+  }
+  clickRegisterCardSelected($event) {
+    $event.preventDefault();
+    console.log($event.target)
+    if($event.target.className!="front"&&$event.target.className!=null){//SI tiene un ID valido pues
+      console.log("Revelo")
+      if(!this.state.solved.includes(cards_info.find(card => card.id==$event.target.className))){
+        if (this.state.first_card_flipped==null){
+          //No se ha volteado ninguna carta
+          console.log($event.target.className)
+          this.state.flipped.set(cards_info.find(card => card.id==$event.target.className).id, true)
+          console.log("nueva primera carta")
+          this.state.first_card_flipped= cards_info.find(card => card.id==$event.target.className)
+          this.state.count_cards_flipped +=1
+          
+        }else{
+          console.log($event.target.className)
+          console.log("segunda carta")
+          //Se esta volteando la segunda carta
+          this.state.flipped.set(cards_info.find(card => card.id==$event.target.className).id, true)
+          if (this.state.first_card_flipped.pair_id
+            ==cards_info.find(card => card.id==$event.target.className).id)
+          {
+            this.state.coincidio = true
+            this.state.points +=1
+            this.state.solved.push(cards_info.find(card => card.id==$event.target.className).id)
+            this.state.solved.push((this.state.first_card_flipped.id))
+          }else{
+              //No coincide la segunda carta
+              console.log("no coincidio")
+              this.state.coincidio = false
+              //Esto tengo que hacer que lo haga después de un tiempo, para que se vea la segunda carta
+              this.resetFlip(cards_info.find(card => card.id==$event.target.className).id,this.state.first_card_flipped.id )
+            
+          }
+          this.state.first_card_flipped= null
+          
+        }
+      }
+      console.log(this.state.flipped)
+    } else{
+      console.log("Escondio")
+    }
+    if(this.state.points>=6){
+      console.log("Gano")
+      this.state.win = true
+    }
+  }
+  render() {
     return (
       <>
       <header>
@@ -51,15 +96,39 @@ class Game extends React.Component {
           </div>
           <div className='div-text' id="record-div">
             <div>
-              Puntos: 0/6 
+              Puntos: {this.state.points}/6 
             </div>
           </div>          
       </header>
-      <div id="game-grid">
-        {this.state.setup.map(id =>{
-          return <div key={id} className="card-container"  onClick={()=>this.setState({moves: this.state.moves + 1})}><Card cardid={id}/></div>
-        })}
-
+      <div style={{ postion: "relative" }}>
+        <div id="game-grid">
+          {this.state.setup.map((id, index) =>{
+            return <div key={id} className="card-container"
+            onClick={($event)=>{
+              this.setState({moves: this.state.moves + 1})
+              this.clickRegisterCardSelected($event)
+            }
+          }><Card cardid={id}  flip={this.state.flipped.get(id)}/></div>
+          
+          })}
+          <div style={{
+            display:this.state.win? "auto": "none",
+            position: this.state.win?"absolute":"auto", 
+            width: this.state.win?"100%":"auto", 
+            height: this.state.win?"100%":"auto",
+            background: this.state.win?"rgba(128, 128, 128, 0.4)":"auto",
+            display: this.state.win?"flex":"auto",
+            justifyContent: this.state.win?"center":"auto"
+            }} >
+            <div id="win-dialogue">
+              <img src= {YouWin} id="you-win-image"/>
+              <Link to="/">
+                <img src= {BackToMenu} id="main-menu-image"/>
+              </Link>
+            </div>
+          </div>
+        </div>
+   
       </div>
       </>
     )
