3090e3e1dd30dee01276c26fca9d6b7744b3a2ab
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-04 18:59:13 -0600

    createRoot with React18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import {createRoot} from "react-dom/client"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './index.css'
 import Game from "./components/comp_game/Game"
 import Menu from "./components/comp_menu/Menu"
-
-
-ReactDOM.render(
-    <>
+const container = document.getElementById('root')
+const root = createRoot(container)
+const App = ()=>{
+    return <div>
     <Router>    
         <Routes>
             <Route path="/" element = {<Menu />}/>
             <Route path="/game" element = {<Game />}/>
         </Routes>    
     </Router>
-    </>,
-    document.getElementById("root"))
+    </div>;
+}
+root.render(<App/>)
