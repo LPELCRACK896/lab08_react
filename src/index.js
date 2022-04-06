eb68724dfdca74e42900a8bbe2182962c0858328
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-06 17:42:06 -0600

    Implements Linters

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react"
-import {createRoot} from "react-dom/client"
+import React from 'react'
+import {createRoot} from 'react-dom/client'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './index.css'
-import Game from "./components/comp_game/Game"
-import Menu from "./components/comp_menu/Menu"
+import Game from './components/comp_game/Game'
+import Menu from './components/comp_menu/Menu'
 const container = document.getElementById('root')
 const root = createRoot(container)
 const App = () =>{
