ff4d3895148423d9d1d96f5c683e6e4c958d0ff5
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-06 20:32:10 -0600

    Fix routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {createRoot} from "react-dom/client"
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Routes, Route, HashRouter} from 'react-router-dom'
 import './index.css'
 import Game from "./components/comp_game/Game"
 import Menu from "./components/comp_menu/Menu"
@@ -8,12 +8,12 @@ const container = document.getElementById('root')
 const root = createRoot(container)
 const App = () =>{
     return <div>
-    <Router>    
+    <HashRouter>    
         <Routes>
-            <Route path="/" element = {<Menu />}/>
-            <Route path="/game" element = {<Game />}/>
+            <Route exact path="/" element = {<Menu />}/>
+            <Route exact path="/game" element = {<Game />}/>
         </Routes>    
-    </Router>
+    </HashRouter>
     </div>;
 }
 root.render(<App/>)
